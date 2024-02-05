b70dfedb4169c2475159b7677e5fb1369901c41c
FhdRasheed <145442940+FhdRasheed@users.noreply.github.com>
2024-02-05 11:04:11 +0530

    Add files via upload

---
+++ stddetails.sh
@@ -0,0 +1,17 @@
+echo "Enter student Details"
+echo "Enter Name "
+read name
+echo "Enter your Semester"
+read sem
+echo enter batch
+read batch
+echo "enter roll number"
+read rollno
+echo enter register number
+read regno
+echo "Name:$name"
+echo "Sem:$sem"
+echo "Batch:$batch"
+echo "Rollno:$rollno"
+echo "Regno:$regno"
+
